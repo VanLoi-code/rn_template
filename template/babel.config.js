8529f8474d955c1cbc0510b8806ca2c3abdd4a73
LCoder <loidv@midota.com>
2021-11-15 20:13:11 +0700

    commit

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@locale': './src/locale',
+          '@navigator': './src/navigator',
+          '@screens': './src/screens',
+          '@store': './src/store',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+
+          '@router': './src/navigator/router',
+          '@responsive': './src/utils/responsive',
+        },
+      },
+    ],
+  ],
 };
---
+++ template/src/components/base/Pressable/index.js
@@ -0,0 +1,185 @@
+import {
+  handleFlex,
+  handleFlexGrow,
+  handleFlexShrink,
+  handleRound,
+  handleSquare,
+} from '@components/shared';
+import React from 'react';
+import {Pressable as RNPrsesable, StyleSheet} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Text from '../Text';
+import styles from './styles';
+import {hs, vs} from '@responsive';
+import {COLORS} from '@theme';
+
+const Pressable = ({
+  //layout
+  flex,
+  flexGrow,
+  flexShrink,
+  row,
+  column,
+  space,
+  wrap,
+  rowCenter,
+  alignStart,
+  alignCenter,
+  alignEnd,
+  justifyStart,
+  justifyCenter,
+  justifyEnd,
+  overflow,
+  alignSelf,
+  zIndex,
+  //size
+  padding,
+  margin,
+  paddingTop,
+  paddingRight,
+  paddingBottom,
+  paddingLeft,
+  marginTop,
+  marginRight,
+  marginBottom,
+  marginLeft,
+  paddingVertical,
+  paddingHorizontal,
+  marginVertical,
+  marginHorizontal,
+  radius,
+  borderTopLeftRadius,
+  borderTopRightRadius,
+  borderWidth,
+  borderTopWidth,
+  borderRightWidth,
+  borderBottomWidth,
+  borderLeftWidth,
+  translateY,
+  safeAreaTop,
+  safeAreaBottom,
+  //width, height
+  width,
+  height,
+  maxWidth,
+  maxHeight,
+  square,
+  round,
+  //position
+  relative,
+  absolute,
+  top,
+  left,
+  right,
+  bottom,
+  //color
+  backgroundColor,
+  borderColor,
+  color,
+  opacity,
+  //shadow
+  shadow1,
+  shadow2,
+  shadow3,
+
+  labelProps,
+  onPress,
+  onLayout,
+  style,
+  children,
+  ...rest
+}) => {
+  const insets = useSafeAreaInsets();
+
+  const customStyles = [
+    // layout
+    flex && handleFlex(flex),
+    flexShrink && handleFlexShrink(flexShrink),
+    flexGrow && handleFlexGrow(flexGrow),
+    row && {flexDirection: 'row'},
+    column && {flexDirection: 'column'},
+    space && {justifyContent: `space-${space}`},
+    wrap && {flexWrap: 'wrap'},
+    rowCenter && styles.rowCenter,
+    alignStart && {alignItems: 'flex-start'},
+    alignCenter && {alignItems: 'center'},
+    alignEnd && {alignItems: 'flex-end'},
+    justifyStart && {justifyContent: 'flex-start'},
+    justifyCenter && {justifyContent: 'center'},
+    justifyEnd && {justifyContent: 'flex-end'},
+    overflow && {overflow},
+    alignSelf && {alignSelf},
+    zIndex && {zIndex},
+    //size
+    padding && {padding: hs(padding)},
+    margin && {margin: hs(margin)},
+    paddingTop && {paddingTop: hs(paddingTop)},
+    paddingRight && {paddingRight: hs(paddingRight)},
+    paddingBottom && {paddingBottom: hs(paddingBottom)},
+    paddingLeft && {paddingLeft: hs(paddingLeft)},
+    marginTop && {marginTop: hs(marginTop)},
+    marginRight && {marginRight: hs(marginRight)},
+    marginBottom && {marginBottom: hs(marginBottom)},
+    marginLeft && {marginLeft: hs(marginLeft)},
+    paddingVertical && {paddingVertical: hs(paddingVertical)},
+    paddingHorizontal && {paddingHorizontal: hs(paddingHorizontal)},
+    marginVertical && {marginVertical: hs(marginVertical)},
+    marginHorizontal && {marginHorizontal: hs(marginHorizontal)},
+    radius && {borderRadius: hs(radius)},
+    borderTopLeftRadius && {borderTopLeftRadius: hs(borderTopLeftRadius)},
+    borderTopRightRadius && {borderTopRightRadius: hs(borderTopRightRadius)},
+    borderWidth && {borderWidth: hs(borderWidth)},
+    borderTopWidth && {borderTopWidth: hs(borderTopWidth)},
+    borderRightWidth && {borderRightWidth: hs(borderRightWidth)},
+    borderBottomWidth && {borderBottomWidth: hs(borderBottomWidth)},
+    borderLeftWidth && {borderLeftWidth: hs(borderLeftWidth)},
+    translateY && {transform: [{translateY: hs(translateY)}]},
+    safeAreaTop && {marginTop: insets.top},
+    safeAreaBottom && {marginBottom: insets.bottom},
+    //width, height
+    width && {width: hs(width)},
+    height && {height: vs(height)},
+    maxWidth && {maxWidth: hs(width)},
+    maxHeight && {maxHeight: vs(height)},
+    round && handleRound(hs(round)),
+    square && handleSquare(hs(square)),
+    //position
+    relative && {position: 'relative'},
+    absolute && {position: 'absolute'},
+    top !== undefined && {top: hs(top)},
+    right !== undefined && {right: hs(right)},
+    bottom !== undefined && {bottom: hs(bottom)},
+    left !== undefined && {left: hs(left)},
+    //color
+    backgroundColor && {
+      backgroundColor: COLORS[backgroundColor] || backgroundColor,
+    },
+    borderColor && {
+      borderColor: COLORS[borderColor] || borderColor,
+    },
+    color && {color},
+    opacity !== undefined && {opacity},
+    //shadow
+    shadow1 && styles.shadow1,
+    shadow2 && styles.shadow2,
+    shadow3 && styles.shadow3,
+
+    {...StyleSheet.flatten(style)},
+  ];
+
+  return (
+    <RNPrsesable
+      {...rest}
+      style={customStyles}
+      onPress={onPress}
+      onLayout={onLayout}>
+      {typeof children === 'string' ? (
+        <Text {...labelProps}>{children}</Text>
+      ) : (
+        children
+      )}
+    </RNPrsesable>
+  );
+};
+
+export default Pressable;
